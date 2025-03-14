63e2d77509da3926381e4ae0e1e60c326b3c837b
rlatldn1224 <kswksw0322@naver.com>
2025-03-14 14:23:26 +0900

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,19 @@
+public class Hello {
+            
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello!! My First java coding!!");
+        System.out.println("Hello!! My Second java coding!!");
+        System.out.println("Hello!! My Third java coding!!");
+        System.out.println("Hello!! My fourth java coding!!");
+        System.out.println("Hello!! My fifth java coding!!");
+        System.out.println("Hello!! My sixth java coding!!");
+        System.out.println("Hello!! My seventh java coding!!");
+        System.out.println("Hello!! My eighth java coding!!");
+        System.out.println("Hello!! My ninth java coding!!");
+        System.out.println("Hello!! My last java coding!!");
+
+
+    }
+}
